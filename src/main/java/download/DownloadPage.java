9e83f6aa65838f44ff1434eefc96f876e4969aec
Kwan <Chomchomcapital@gmail.com>
2020-07-01 18:04:50 +0700

    download NH bank

---
+++ src/main/java/download/DownloadPage.java
@@ -0,0 +1,39 @@
+package download;
+
+import com.sun.jndi.toolkit.url.Uri;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by https://github.com/kwanpham
+ */
+public class DownloadPage {
+    public static void main(String args[]) throws IOException {
+        download("https://vn.nhbankglobal.com/css/common.css");
+    }
+    public static void download(String urlString) throws IOException {
+        URL url = new URL(urlString);
+
+        try(
+                BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\MyPC\\Desktop\\kqct\\NotoSansKR-Bold.woff2"));
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+            }
+            System.out.println("Page downloaded.");
+        }
+    }
+
+    private String getFileName(URL url) throws MalformedURLException {
+        String path = url.getPath();
+
+    }
+}
